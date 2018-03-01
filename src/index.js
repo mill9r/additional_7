fc3b6dfacf8d7f1666962e0b65c5e18d3aa07fa0
TATJAVA_Andrei_Tsylko <mi113r@mail.ru>
2018-03-01 17:34:16 +0300

    Update index.js
    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  return parseGrid(matrix)
+}
+function parseGrid(grid) {
+    let nGrid;
+    for (let row = 0; row < 9; row++) {
+        for (let col = 0; col < 9; col++) {
+             if (!grid[row][col]) {
+                for (let candidate = 1; candidate < 10; candidate++) {
+                    if (checkRowColumn(grid, row, col, candidate) && checkBox(grid, row, col, candidate)) {
+                        grid[row][col] = candidate;
+                        nGrid = parseGrid(grid);
+                        if (nGrid) {
+                            return grid
+                        }
+                        grid[row][col] = 0;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function checkRowColumn(grid, row, col, candidate) {
+    for (let i = 0; i < 9; i++) {
+        if (i != row && grid[i][col] == candidate || i != col && grid[row][i] == candidate) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function checkBox(grid, row, col, candidate) {
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (i != row && j != col && grid[Math.floor((row / 3)) * 3 + i][Math.floor((col / 3)) * 3 + j] == candidate) {
+                return false;
+            }
+        }
+    }
+    return true
 }
